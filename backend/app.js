ce703489351545a14f390de595a58f59f315998e
Jesse <jesse.movecar@gmail.com>
2021-12-15 10:11:14 +0100

    Ajout headers pour pb Cors et 3 dependencies ajoutées  dans le backend + bcrypt

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const userRoutes = require('./routes/user');
+
 mongoose
   .connect(
     'mongodb+srv://Jesse1812:Varader0@cluster0.ooqgv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -11,6 +13,19 @@ mongoose
 
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
@@ -30,5 +45,8 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   console.log('Réponse envoyée avec succès !');
 });
+
+// app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
