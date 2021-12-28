12e6db94adca0f35e97e7c25541231ea7a0aa162
Jesse <jesse.movecar@gmail.com>
2021-12-28 11:10:46 +0100

    Ajout module npm cors

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-
+const cors = require('cors');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
 
@@ -15,12 +15,13 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization,'
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
