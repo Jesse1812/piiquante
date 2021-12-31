ac1d314491a7cbbb2bbf55c573d3cc8b4897e03a
Jesse <jesse.movecar@gmail.com>
2021-12-31 11:07:58 +0100

    remplacement stuff par sauce et effacement fichiers stuff.js

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
-const stuffRoutes = require('./routes/stuff');
+
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
+
+const app = express();
 
 mongoose
   .connect(
@@ -12,10 +15,6 @@ mongoose
   )
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-const app = express();
-app.use(express.json());
-app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,9 +29,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.json());
+app.use(cors());
 
-app.use('/api/stuff', stuffRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
